c1eda56a9e8c801cb67a7d2cfc08069662bd7f92
Arpasiri <arpasiri.se@ku.th>
2024-10-15 02:02:54 +0700

    edit mycard & workflow file

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            AJ Suriya 
+            Arpasiri 
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
